2491f6e96a67f8be43b5744efb0efc4aa2ec2d0f
Andreas Marc Klingler <post@andreas-klingler.de>
2014-04-12 22:15:14 +0200

    Category with place nested.

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,2 +1,3 @@
 class Place < ActiveRecord::Base
+  has_one :category
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140412195313) do
+ActiveRecord::Schema.define(version: 20140412195443) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20140412195313) do
     t.float    "longitude"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "category_id"
   end
 
 end
