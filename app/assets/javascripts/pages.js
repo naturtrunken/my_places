04ce704633b6ea687f5b7fccedf30e89b02af010
Andreas Marc Klingler <post@andreas-klingler.de>
2014-04-12 21:34:49 +0200

    Leaflet configured.

---
+++ app/assets/javascripts/pages.js
@@ -0,0 +1,28 @@
+// Variables for the map
+var map;
+
+$(document).ready(function () {
+    initmap();
+});
+
+// Creates the map.
+// ---------------------------------------------------------------------------------------
+function initmap() {
+
+    // Create the map
+    map = new L.Map('map');
+
+    // Create the OpenStreetMap and Leaflet note.
+    var osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+    var osmAttrib = 'Map data © OpenStreetMap contributors';
+    var osm = new L.TileLayer(osmUrl, {
+        minZoom: 4,
+        maxZoom: 16,
+        attribution: osmAttrib
+    });
+    map.addLayer(osm);
+
+    map.setView(new L.LatLng(52.518611, 13.408056), 11);
+}
+
+
