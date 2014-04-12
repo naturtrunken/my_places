203f7f64b1f5007ffa6ea42e310dd1dd74571041
Andreas Marc Klingler <post@andreas-klingler.de>
2014-04-12 22:16:55 +0200

    Note to place added.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140412195443) do
+ActiveRecord::Schema.define(version: 20140412201525) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20140412195443) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "category_id"
+    t.text     "note"
   end
 
 end
