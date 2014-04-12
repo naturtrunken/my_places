a3b0eddde30901cdf64128b4132c35b91c61bc24
Andreas Marc Klingler <post@andreas-klingler.de>
2014-04-12 21:54:20 +0200

    Categories added.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :categories
+
   root 'pages#index'
   resources :places
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140412191350) do
+ActiveRecord::Schema.define(version: 20140412195313) do
+
+  create_table "categories", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "places", force: true do |t|
     t.string   "name"
