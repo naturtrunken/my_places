ad36e9f31c68e1a58fab7e230479f2eb7fe5cc6a
Andreas Marc Klingler <post@andreas-klingler.de>
2014-04-15 21:51:01 +0200

    Seed file for the markers added and some new marker colours.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Marker.create(
+    name: 'Blue',
+    icon: 'marker-icon'
+)
+
+Marker.create(
+    name: 'Brown',
+    icon: 'marker-icon-brown'
+)
+
+Marker.create(
+    name: 'Cyan',
+    icon: 'marker-icon-cyan'
+)
+
+Marker.create(
+    name: 'Green',
+    icon: 'marker-icon-green'
+)
+
+Marker.create(
+    name: 'Pink',
+    icon: 'marker-icon-pink'
+)
+
+Marker.create(
+    name: 'Purple',
+    icon: 'marker-icon-purple'
+)
+
+Marker.create(
+    name: 'Red',
+    icon: 'marker-icon-red'
+)
+
+Marker.create(
+    name: 'Yellow',
+    icon: 'marker-icon-yellow'
+)
+
