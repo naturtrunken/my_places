57c96445ee34ccb5385fb38ce01cfffbba5a756f
Andreas Marc Klingler <post@andreas-klingler.de>
2014-06-22 12:06:27 +0200

    Settings for the default map position moved in the global configuration file.

--- config/initializers/global.rb
+++ config/initializers/global.rb
@@ -2,6 +2,12 @@
 
 Rails.configuration.global = {
 
+    :map => {
+        :default_latitude => 52.518611,
+        :default_longitude => 13.408056,
+        :default_zoom_level => 11
+    },
+
     :category => {
         :model => {
             :name => {
